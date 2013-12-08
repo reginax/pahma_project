61cd4234c8c0d48c5ada61e135b0543fd233caa2
jblowe <jblowe@berkeley.edu>
2013-12-08 09:37:03 -0800

    PAHMA-979: incremental improvements to BMU webapp

---
+++ uploadmedia/utils.py
@@ -0,0 +1,95 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+import csv
+import codecs
+import time, datetime
+
+tempimagedir = "/tmp/upload_cache/%s"
+jobdir = "/tmp/upload_cache/%s.step1.csv"
+
+tempimagedir = "/tmp/%s"
+jobdir = "/tmp/%s.step1.csv"
+
+
+def getDropdowns():
+    return {
+        'creators':
+            {
+                "Natasha Johnson": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(7652)'Natasha Johnson'",
+                "Elizabeth Minor": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(7500)'Elizabeth Minor'"
+            },
+        'rightsholders':
+            {
+                "PAHMA": "urn:cspace:pahma.cspace.berkeley.edu:orgauthorities:name(organization):item:name(8107)'Phoebe A. Hearst Museum of Anthropology'",
+                "Regents of UC": "urn:cspace:pahma.cspace.berkeley.edu:orgauthorities:name(organization):item:name(6390)'University of California at Berkeley Regents'",
+                "Academy of Art University": "urn:cspace:pahma.cspace.berkeley.edu:orgauthorities:name(organization):item:name(8544)'Academy of Art University'"
+            }
+    }
+
+
+# following function take from stackoverflow...thanks!
+def get_exif(fn):
+    ret = {}
+    i = Image.open(fn)
+    info = i._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
+
+
+def getNumber(filename):
+    objectnumber = filename.split('_')[0]
+    return objectnumber
+
+
+def getCSID(objectnumber):
+    # dummy function, for now
+    objectCSID = objectnumber
+    return objectCSID
+
+
+def writeCsv(filename, items, writeheader):
+    filehandle = codecs.open(filename, 'w', 'utf-8')
+    writer = csv.writer(filehandle, delimiter='\t')
+    writer.writerow(writeheader)
+    for item in items:
+        row = []
+        for x in writeheader:
+            if x in item.keys():
+                cell = item[x]
+            else:
+                cell = ''
+            row.append(cell)
+        writer.writerow(row)
+    filehandle.close()
+
+
+# following function borrowed from Django docs, w modifications
+def handle_uploaded_file(f, imageinfo):
+    destination = open(tempimagedir % f.name, 'wb+')
+    with destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    destination.close()
+
+
+def assignValue(defaultValue, override, imageData, exifvalue, refnameList):
+    if override == 'always':
+        return defaultValue, refnameList.get(defaultValue, defaultValue)
+    elif exifvalue in imageData:
+        imageValue = imageData[exifvalue]
+        # a bit of cleanup
+        imageValue = imageValue.strip()
+        imageValue = imageValue.replace('"','')
+        imageValue = imageValue.replace('\n','')
+        imageValue = imageValue.replace('\r','')
+        return imageData[exifvalue], refnameList.get(imageValue, imageValue)
+    elif override == 'ifblank':
+        return defaultValue, refnameList.get(defaultValue, defaultValue)
+    else:
+        return '', ''
+
+
+def getJobfile(jobnumber):
+    return jobdir % jobnumber
--- uploadmedia/views.py
+++ uploadmedia/views.py
@@ -1,120 +1,89 @@
 __author__ = 'jblowe'
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.conf import settings
 from django import forms
-from PIL import Image
-from PIL.ExifTags import TAGS
-import csv
-import codecs
 import time, datetime
+from utils import getDropdowns, handle_uploaded_file, assignValue, getCSID, getNumber, get_exif, writeCsv, getJobfile
 
-tempimagedir = "/tmp/upload_cache/%s"
-jobdir = "/tmp/upload_cache/%s.csv"
+TITLE = 'Bulk Media Upload'
 
-#tempimagedir = "/tmp/%s"
-#jobdir = "/tmp/%s.csv"
-
-
-# following function take from stackoverflow...thanks!
-def get_exif(fn):
-    ret = {}
-    i = Image.open(fn)
-    info = i._getexif()
-    for tag, value in info.items():
-        decoded = TAGS.get(tag, tag)
-        ret[decoded] = value
-    return ret
-
-
-def getNumber(filename):
-    objectnumber = filename.split('_')[0]
-    return objectnumber
-
-
-def getCSID(objectnumber):
-    objectCSID = objectnumber
-    return objectCSID
-
-
-def writeCsv(filename, items, writeheader):
-    filehandle = codecs.open(filename, 'w', 'utf-8')
-    writer = csv.writer(filehandle, delimiter='\t')
-    writer.writerow(writeheader)
-    for item in items:
-        row = []
-        for x in writeheader:
-            if x in item.keys():
-                cell = item[x]
-            else:
-                cell = ''
-            row.append(cell)
-        writer.writerow(row)
-    filehandle.close()
-
-
-# following function borrowed from Django docs, w modifications
-def handle_uploaded_file(f, imageinfo):
-    destination = open(tempimagedir % f.name, 'wb+')
-    with destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-    destination.close()
-
-
-def assignValue(defaultValue,imageData,exifvalue):
-    if exifvalue in imageData:
-        return imageData[exifvalue]
-    elif defaultValue:
-        return defaultValue
-    else:
-        return ''
-
-@login_required()
+#@login_required()
 def uploadfiles(request):
-    TITLE = 'Select Files to Upload'
+
     jobinfo = {}
     constants = {}
     images = []
+    dropdowns = getDropdowns()
 
     form = forms.Form(request)
     if request.POST:
 
         contributor = request.POST['contributor']
-        creator = request.POST['creator']
-        rightsholder = request.POST['rightsholder']
-        constants = {'creator': creator, 'contributor': contributor, 'rightsholder': rightsholder}
-        for id,afile in enumerate(request.FILES.getlist('imagefiles')):
+        overrideContributor = request.POST['overridecreator']
+
+        creatorDisplayname = request.POST['creator']
+        overrideCreator = request.POST['overridecreator']
+        defaultCreator = ''
+        if creatorDisplayname in dropdowns['creators']:
+            defaultCreator = dropdowns['creators'][creatorDisplayname]
+
+        rightsholderDisplayname = request.POST['rightsholder']
+        overrideRightsholder = request.POST['overriderightsholder']
+        defaultRightsholder = ''
+        if creatorDisplayname in dropdowns['rightsholders']:
+            defaultRightsholder = dropdowns['rightsholders'][rightsholderDisplayname]
+
+        constants = {'creator': creatorDisplayname, 'contributor': contributor, 'rightsholder': rightsholderDisplayname}
+
+        for lineno,afile in enumerate(request.FILES.getlist('imagefiles')):
             #print afile
             try:
-                print "%s %s: %s %s (%s %s)" % ('id', id, 'name', afile.name, 'size', afile.size)
+                print "%s %s: %s %s (%s %s)" % ('id', lineno, 'name', afile.name, 'size', afile.size)
                 im = get_exif(afile)
                 objectnumber = getNumber(afile.name)
-                objectCSID = getCSID(objectnumber)
-                creator = assignValue(creator,im,'Artist')
-                contributor = assignValue(contributor,im,'ImageDescription')
-                rightsholder = assignValue(rightsholder,im,'RightsHolder')
-                datetimedigitized = assignValue('',im,'DateTimeDigitized')
-                imageinfo = {'id': id, 'name': afile.name, 'size': afile.size, 'objectnumber': objectnumber, 'objectCSID': objectCSID,
-                             'date': datetimedigitized, 'creator': creator,
-                             'contributor': contributor, 'rightsholder': rightsholder}
-                images.append(imageinfo)
-                #images.append([afile.name,afile.size])
+                #objectCSID = getCSID(objectnumber)
+                creator, creatorRefname = assignValue(creatorDisplayname,overrideCreator,im,'Artist',dropdowns['creators'])
+                contributor, dummy = assignValue(contributor,overrideContributor,im,'ImageDescription',{})
+                rightsholder, rightsholderRefname = assignValue(rightsholderDisplayname,overrideRightsholder,im,'RightsHolder',dropdowns['rightsholders'])
+                datetimedigitized, dummy = assignValue('','ifblank',im,'DateTimeDigitized',{})
+                imageinfo = {'id': lineno, 'name': afile.name, 'size': afile.size,
+                             'objectnumber': objectnumber,
+                             #'objectCSID': objectCSID,
+                             'date': datetimedigitized,
+                             'creator': creatorRefname,
+                             'contributor': contributor,
+                             'rightsholder': rightsholderRefname,
+                             'creatorDisplayname': creator,
+                             'rightsholderDisplayname': rightsholder,
+                             'contributorDisplayname': contributor
+                }
                 handle_uploaded_file(afile, imageinfo)
+                images.append(imageinfo)
             except:
+                #raise
                 images.append({'name': afile.name, 'size': afile.size, 'error': 'problem extracting image metadata, not processed'})
 
         if len(images) > 0:
             jobnumber = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
             jobinfo['jobnumber'] = jobnumber
-            writeCsv(jobdir % jobnumber, images,
-                     ['name', 'size', 'objectnumber', 'objectCSID', 'date', 'contributor', 'rightsholder'])
+            writeCsv(getJobfile(jobnumber), images,
+                     ['name', 'size', 'objectnumber', 'date', 'creator', 'contributor', 'rightsholder'])
             jobinfo['estimatedtime'] = '%8.1f' % (len(images) * 10 / 60.0)
+
+            if 'createmedia' in request.POST:
+                jobinfo['status'] = 'createmedia'
+            elif 'uploadmedia' in request.POST:
+                jobinfo['status'] = 'uploadmedia'
+            else:
+                jobinfo['status'] = 'No status possible'
 
     status = 'up'
     timestamp = time.strftime("%b %d %Y %H:%M:%S", time.localtime())
+    overrides = [['ifblank', 'Overide only if blank'],
+               ['always', 'Always Overide']]
 
     return render(request, 'uploadmedia.html',
                   {'title': TITLE, 'images': images, 'count': len(images), 'constants': constants, 'jobinfo': jobinfo,
-                   'status': status, 'timestamp': timestamp})
+                   'dropdowns': dropdowns, 'overrides': overrides, 'status': status, 'timestamp': timestamp})
