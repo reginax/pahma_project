89a926cc4595a915bf79ad40787b3d9e7798967b
jblowe <jblowe@berkeley.edu>
2013-12-12 14:37:23 -0800

    PAHMA-979: repair urls for queue display v2...

--- uploadmedia/urls.py
+++ uploadmedia/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import patterns, url
 from uploadmedia import views
 
 urlpatterns = patterns('',
-                       url(r'^uploadfiles$', views.uploadfiles, name='uploadfiles'),
-                       url(r'^showqueue$', views.showqueue, name='showqueue'),
+                       url(r'^uploadfiles', views.uploadfiles, name='uploadfiles'),
+                       url(r'^showqueue', views.showqueue, name='showqueue'),
                        url(r'^showresults/(?P<filename>[\w\-\.]+)$', views.showresults, name='showresults'),
                        #url(r'createmedia', views.createmedia, name='createmedia'),
                        )
