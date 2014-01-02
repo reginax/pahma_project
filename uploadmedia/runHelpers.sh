af2f2882f22bf34ded4967080eda2b20149a6162
jblowe <jblowe@berkeley.edu>
2014-01-01 20:45:13 -0800

    PAHMA-979: make the helpers more useful -- a script that actually runs them.

--- uploadmedia/runHelpers.sh
+++ uploadmedia/runHelpers.sh
@@ -1,4 +1,5 @@
-#cp /home/developers/pahma/4solr.pahma.media.csv media.csv
+cp /home/developers/pahma/4solr.pahma.media.csv media.csv
+perl checkRuns.pl csids > csids.csv
 perl checkObj.pl > MediaNBlobs.txt
 grep OK MediaNBlobs.txt > OK.txt
 grep 'NoMatch|None' MediaNBlobs.txt  | grep -v NoObjectFound> NotInDB.txt
@@ -9,6 +10,6 @@ for f in `cat NotInDB.jpgfilenames.txt ` ; do grep  -h "$f" /tmp/upload_cache/*.
 for f in `cat NotInDB.jpgfilenames.txt ` ; do grep  "$f" /tmp/upload_cache/*.step2.csv ; done | sort -u > NotInDB.step2.txt  
 sort -u NotInDB.step2.txt  | cut -f1 -d":" | sort | uniq -c >RerunFiles.txt
 cat NoObjectFound.txt NotInDB.txt | cut -f1 > ToCheck.txt
-./verifyObjectsAndMedia.sh < ToCheck.txt > ToCheck.results.txt &
+./verifyObjectsAndMedia.sh < ToCheck.txt > ToCheck.results.txt 
 perl -ne 'print if /\|\s*$/' ToCheck.results.txt > NoObjectFoundv2.txt 
 wc -l *.txt
