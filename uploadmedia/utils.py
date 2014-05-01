3ec03513219ec0ada4fac5f5b5296374c022d0c0
jblowe <jblowe@berkeley.edu>
2014-04-30 17:30:59 -0700

    PAHMA-1100: update Creators list

--- uploadmedia/utils.py
+++ uploadmedia/utils.py
@@ -67,7 +67,6 @@ def getDropdowns():
         'creators':
             {
                 "Natasha Johnson": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(7652)'Natasha Johnson'",
-                "Rowan Gard": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(RowanGard1342219780674)'Rowan Gard'",
                 "Alicja Egbert": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(8683)'Alicja Egbert'",
                 "Paolo Pellegatti": "urn:cspace:pahma.cspace.berkeley.edu:personauthorities:name(person):item:name(8020)'Paolo Pellegatti'"
             },
