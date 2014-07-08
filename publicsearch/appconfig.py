b9e592b082f4759ade889ef0df71a72be7a4ec21
jblowe <jblowe@berkeley.edu>
2014-07-08 08:12:20 -0700

    PAHMA-1083: incremental improvements: maps now work, links in results, etc.

--- publicsearch/appconfig.py
+++ publicsearch/appconfig.py
@@ -4,7 +4,6 @@ from os import path
 from common import cspace # we use the config file reading function
 from cspace_django_site import settings
 import csv
-import traceback
 
 
 def getParms(parmFile):
@@ -66,45 +65,39 @@ def parseRows(rows):
 
     return FIELDS,PARMS
 
-try:
-    config = cspace.getConfig(path.join(settings.BASE_PARENT_DIR, 'config'), 'search')
+config = cspace.getConfig(path.join(settings.BASE_PARENT_DIR, 'config'), 'search')
 
-    MAXMARKERS = int(config.get('search', 'MAXMARKERS'))
-    MAXRESULTS = int(config.get('search', 'MAXRESULTS'))
-    MAXLONGRESULTS = int(config.get('search', 'MAXLONGRESULTS'))
-    MAXFACETS = int(config.get('search', 'MAXFACETS'))
-    EMAILABLEURL = config.get('search', 'EMAILABLEURL')
-    IMAGESERVER = config.get('search', 'IMAGESERVER')
-    BMAPPERSERVER = config.get('search', 'BMAPPERSERVER')
-    BMAPPERDIR = config.get('search', 'BMAPPERDIR')
-    BMAPPERCONFIGFILE = config.get('search', 'BMAPPERCONFIGFILE')
-    SOLRSERVER = config.get('search', 'SOLRSERVER')
-    SOLRCORE = config.get('search', 'SOLRCORE')
-    LOCALDIR = config.get('search', 'LOCALDIR')
-    SEARCH_QUALIFIERS = config.get('search', 'SEARCH_QUALIFIERS').split(',')
-    FIELDDEFINITIONS = config.get('search', 'FIELDDEFINITIONS')
-    CSVPREFIX = config.get('search', 'CSVPREFIX')
-    CSVEXTENSION = config.get('search', 'CSVEXTENSION')
+MAXMARKERS = int(config.get('search', 'MAXMARKERS'))
+MAXRESULTS = int(config.get('search', 'MAXRESULTS'))
+MAXLONGRESULTS = int(config.get('search', 'MAXLONGRESULTS'))
+MAXFACETS = int(config.get('search', 'MAXFACETS'))
+EMAILABLEURL = config.get('search', 'EMAILABLEURL')
+IMAGESERVER = config.get('search', 'IMAGESERVER')
+BMAPPERSERVER = config.get('search', 'BMAPPERSERVER')
+BMAPPERDIR = config.get('search', 'BMAPPERDIR')
+BMAPPERCONFIGFILE = config.get('search', 'BMAPPERCONFIGFILE')
+SOLRSERVER = config.get('search', 'SOLRSERVER')
+SOLRCORE = config.get('search', 'SOLRCORE')
+LOCALDIR = config.get('search', 'LOCALDIR')
+SEARCH_QUALIFIERS = config.get('search', 'SEARCH_QUALIFIERS').split(',')
+FIELDDEFINITIONS = config.get('search', 'FIELDDEFINITIONS')
+CSVPREFIX = config.get('search', 'CSVPREFIX')
+CSVEXTENSION = config.get('search', 'CSVEXTENSION')
 
-    # get "frontend" configuration from the ... frontend configuaration file FIELDDEFINITIONS
+# get "frontend" configuration from the ... frontend configuaration file FIELDDEFINITIONS
 
-    print 'reading field definitions from %s' % path.join(settings.BASE_PARENT_DIR, 'config/' + FIELDDEFINITIONS)
+print 'reading field definitions from %s' % path.join(settings.BASE_PARENT_DIR, 'config/' + FIELDDEFINITIONS)
 
-    FIELDS, PARMS = getParms(path.join(settings.BASE_PARENT_DIR, 'config/' + FIELDDEFINITIONS))
+FIELDS, PARMS = getParms(path.join(settings.BASE_PARENT_DIR, 'config/' + FIELDDEFINITIONS))
 
-    LOCATION = ''
+LOCATION = ''
 
-    DROPDOWNS = []
-    for p in PARMS:
-        if 'dropdown' in PARMS[p][1]:
-            DROPDOWNS.append(PARMS[p][4])
-        if 'location' in PARMS[p][1]:
-            LOCATION = PARMS[p][3]
+DROPDOWNS = []
+for p in PARMS:
+    if 'dropdown' in PARMS[p][1]:
+        DROPDOWNS.append(PARMS[p][4])
+    if 'location' in PARMS[p][1]:
+        LOCATION = PARMS[p][3]
 
-    if LOCATION == '':
-        print "LOCATION not set, please specify a variable as 'location'"
-
-except:
-    print "Had a problem reading %s." % path.join(settings.BASE_PARENT_DIR, 'config')
-    print "the error is: %s" % traceback.format_exc()
-    print "This Search webapp will not run."
+if LOCATION == '':
+    print "LOCATION not set, please specify a variable as 'location'"
