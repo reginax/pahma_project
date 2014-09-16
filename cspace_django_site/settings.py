1166c7e46dbd6c5808ba9af7dd19e985dceb263b
jblowe <jblowe@berkeley.edu>
2014-09-15 20:22:03 -0700

    NOJIRA: generalizing logo presentation and landing page

--- cspace_django_site/settings.py
+++ cspace_django_site/settings.py
@@ -244,7 +244,7 @@ else:
 
 logging.debug('WSGI_BASE =' + WSGI_BASE)
 LOGIN_URL = WSGI_BASE + '/accounts/login'
-LOGIN_REDIRECT_URL = WSGI_BASE + '/search/search'
+LOGIN_REDIRECT_URL = WSGI_BASE + '/landing'
 
 #
 # AuthN backends
