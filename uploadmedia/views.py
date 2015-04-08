8a7556a4e4ce5549e6a4590eeb6f205aaae190fd
John Lowe <jblowe@berkeley.edu>
2015-04-08 15:03:45 -0700

    NOJIRA: print this project up to date

--- uploadmedia/views.py
+++ uploadmedia/views.py
@@ -6,18 +6,19 @@ from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
 from django import forms
 import time, datetime
-from utils import SERVERINFO, getDropdowns, handle_uploaded_file, assignValue, getCSID, getNumber, get_exif, writeCsv, getJobfile, getJoblist, loginfo, getQueue
+from utils import SERVERINFO, getDropdowns, handle_uploaded_file, assignValue, getCSID, getNumber, get_exif, writeCsv, \
+    getJobfile, getJoblist, loginfo, getQueue
 import subprocess
-import os,sys
+import os, sys
 
 TITLE = 'Bulk Media Upload'
 
 overrides = [['ifblank', 'Overide only if blank'],
              ['always', 'Always Overide']]
 
+
 @login_required()
 def uploadfiles(request):
-
     jobinfo = {}
     constants = {}
     images = []
@@ -38,17 +39,19 @@ def uploadfiles(request):
 
         constants = {'creator': creatorDisplayname, 'contributor': contributor, 'rightsholder': rightsholderDisplayname}
 
-        for lineno,afile in enumerate(request.FILES.getlist('imagefiles')):
-            #print afile
+        for lineno, afile in enumerate(request.FILES.getlist('imagefiles')):
+            # print afile
             try:
                 print "%s %s: %s %s (%s %s)" % ('id', lineno, 'name', afile.name, 'size', afile.size)
                 im = get_exif(afile)
-                objectnumber,imagenumber = getNumber(afile.name)
-                #objectCSID = getCSID(objectnumber)
-                creator, creatorRefname = assignValue(creatorDisplayname,overrideCreator,im,'Artist',dropdowns['creators'])
-                contributor, dummy = assignValue(contributor,overrideContributor,im,'ImageDescription',{})
-                rightsholder, rightsholderRefname = assignValue(rightsholderDisplayname,overrideRightsholder,im,'RightsHolder',dropdowns['rightsholders'])
-                datetimedigitized, dummy = assignValue('','ifblank',im,'DateTimeDigitized',{})
+                objectnumber, imagenumber = getNumber(afile.name)
+                # objectCSID = getCSID(objectnumber)
+                creator, creatorRefname = assignValue(creatorDisplayname, overrideCreator, im, 'Artist',
+                                                      dropdowns['creators'])
+                contributor, dummy = assignValue(contributor, overrideContributor, im, 'ImageDescription', {})
+                rightsholder, rightsholderRefname = assignValue(rightsholderDisplayname, overrideRightsholder, im,
+                                                                'RightsHolder', dropdowns['rightsholders'])
+                datetimedigitized, dummy = assignValue('', 'ifblank', im, 'DateTimeDigitized', {})
                 imageinfo = {'id': lineno, 'name': afile.name, 'size': afile.size,
                              'objectnumber': objectnumber,
                              'imagenumber': imagenumber,
@@ -67,12 +70,13 @@ def uploadfiles(request):
                 raise
                 # we still upload the file, anyway...
                 handle_uploaded_file(afile, imageinfo)
-                images.append({'name': afile.name, 'size': afile.size, 'error': 'problem extracting image metadata, not processed'})
+                images.append({'name': afile.name, 'size': afile.size,
+                               'error': 'problem extracting image metadata, not processed'})
 
         if len(images) > 0:
             jobnumber = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
             jobinfo['jobnumber'] = jobnumber
-            writeCsv(getJobfile(jobnumber)+'.step1.csv', images,
+            writeCsv(getJobfile(jobnumber) + '.step1.csv', images,
                      ['name', 'size', 'objectnumber', 'date', 'creator', 'contributor', 'rightsholder', 'imagenumber'])
             jobinfo['estimatedtime'] = '%8.1f' % (len(images) * 10 / 60.0)
 
@@ -80,11 +84,12 @@ def uploadfiles(request):
                 jobinfo['status'] = 'createmedia'
                 loginfo('start', getJobfile(jobnumber), request)
                 try:
-                    retcode = subprocess.call(["/usr/local/share/django/pahma_project/uploadmedia/postblob.sh", getJobfile(jobnumber)])
+                    retcode = subprocess.call(
+                        ["/usr/local/share/django/pahma_project/uploadmedia/postblob.sh", getJobfile(jobnumber)])
                     if retcode < 0:
-                        loginfo('process', jobnumber+" Child was terminated by signal %s" %  -retcode, request)
+                        loginfo('process', jobnumber + " Child was terminated by signal %s" % -retcode, request)
                     else:
-                        loginfo('process', jobnumber+": Child returned %s" %  retcode, request)
+                        loginfo('process', jobnumber + ": Child returned %s" % retcode, request)
                 except OSError as e:
                     loginfo('error', "Execution failed: %s" % e, request)
                 loginfo('finish', getJobfile(jobnumber), request)
@@ -99,8 +104,10 @@ def uploadfiles(request):
     elapsedtime = time.time() - elapsedtime
 
     return render(request, 'uploadmedia.html',
-                  {'title': TITLE, 'serverinfo': SERVERINFO, 'images': images, 'count': len(images), 'constants': constants, 'jobinfo': jobinfo,
-                   'dropdowns': dropdowns, 'overrides': overrides, 'status': status, 'timestamp': timestamp, 'elapsedtime': '%8.2f' % elapsedtime})
+                  {'title': TITLE, 'serverinfo': SERVERINFO, 'images': images, 'count': len(images),
+                   'constants': constants, 'jobinfo': jobinfo,
+                   'dropdowns': dropdowns, 'overrides': overrides, 'status': status, 'timestamp': timestamp,
+                   'elapsedtime': '%8.2f' % elapsedtime})
 
 
 @login_required()
@@ -114,12 +121,13 @@ def showresults(request, filename):
 @login_required()
 def showqueue(request):
     elapsedtime = time.time()
-    jobs,count = getJoblist()
+    jobs, count = getJoblist()
     dropdowns = getDropdowns()
     elapsedtime = time.time() - elapsedtime
     status = 'up'
     timestamp = time.strftime("%b %d %Y %H:%M:%S", time.localtime())
 
     return render(request, 'uploadmedia.html',
-                  {'dropdowns': dropdowns, 'overrides': overrides, 'timestamp': timestamp, 'elapsedtime': '%8.2f' % elapsedtime,
+                  {'dropdowns': dropdowns, 'overrides': overrides, 'timestamp': timestamp,
+                   'elapsedtime': '%8.2f' % elapsedtime,
                    'status': status, 'title': TITLE, 'serverinfo': SERVERINFO, 'jobs': jobs, 'count': count})
