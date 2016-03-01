138d89271c8f0d2f7f28b26b5ede1cb869d1cfeb
John Lowe <jblowe@berkeley.edu>
2016-03-01 09:49:19 -0800

    NOJIRA: comment out PDF generation code, for now

--- common/utils.py
+++ common/utils.py
@@ -9,9 +9,9 @@ from os import path
 from copy import deepcopy
 
 from io import BytesIO
+# disable reportlab code for now
 # from common.table import makeReport
 from django.http import HttpResponse, HttpResponseRedirect
-# from cspace_django_site.main import cspace_django_site
 
 
 SolrIsUp = True  # an initial guess! this is verified below...
@@ -239,7 +239,7 @@ def setupBMapper(requestObject, context, prmz):
     return context
     # return HttpResponseRedirect(context['bmapperurl'])
 
-def setup4Print(request, context, prmz):
+def setup4PDF(request, context, prmz):
 
     csvformat, fieldset, csvitems = setupCSV(request, context, prmz)
     table = []
@@ -261,15 +261,19 @@ def setup4Print(request, context, prmz):
 
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="My Users.pdf"'
+    response['Content-Disposition'] = 'attachment; filename="test.pdf"'
 
     buffer = BytesIO()
 
-#    report = makeReport(buffer, 'Letter', 'header', 'footer')
-#    pdf = report.fillReport(table)
+    #report = makeReport(buffer, 'Letter', 'header', 'footer')
+    #pdf = report.fillReport(table)
 
-#    response.write(pdf)
+    #response.write(pdf)
     return response
+
+
+def setup4Print(request, context, prmz):
+    return
 
 
 def computeStats(requestObject, context, prmz):
